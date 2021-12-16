d77a998a9be5eba5a52a255c7049d38b2e738933
martinheyne <martinheyne1web.de>
2021-12-16 12:20:36 +0100

    Meeting test

---
+++ src/main/Meeting.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Meeting {
+
+    public static String meeting(String s) {
+        List<String[]> list = new ArrayList<>();
+        char[] chararr = s.toUpperCase().toCharArray();
+        String vorname = "";
+        String nachname = "";
+        Boolean istNachname = Boolean.TRUE;
+        String output = "";
+        for(int i = 0; i < chararr.length; i++){
+            if(chararr[i] == ':'){
+                istNachname = Boolean.FALSE;
+                vorname = "";
+
+            }else if(chararr[i] == ';'){
+                istNachname = Boolean.TRUE;
+                list.add(new String[]{vorname,nachname});
+
+                nachname = "";
+
+            }else{
+                if(istNachname == Boolean.TRUE){
+                    nachname += chararr[i];
+                }else if(istNachname == Boolean.FALSE){
+                    vorname += chararr[i];
+                }
+            }
+
+        }
+        list.add(new String[]{vorname,nachname});
+
+        Collections.sort(list);//https://www.happycoders.eu/de/algorithmen/sortieren-in-java/
+        for(int i = 0; i < list.size(); i++){
+            output += "(";
+            output += list.get(i)[0];
+            output += ", ";
+            output += list.get(i)[1];
+            output += ")";
+        }
+        return output;
+    }
+}
--- src/test/BitsOfIntegerTest.java
+++ src/test/BitsOfIntegerTest.java
@@ -1,14 +1,15 @@
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+import org.junit.jupiter.api.Assertions;
 
 public class BitsOfIntegerTest {
     @Test
     public void fixed_tests() {
-        assertEquals(BitsOfInteger.reverse_bits(417), 267);
-        assertEquals(BitsOfInteger.reverse_bits(267), 417);
-        assertEquals(BitsOfInteger.reverse_bits(0), 0);
-        assertEquals(BitsOfInteger.reverse_bits(2017), 1087);
-        assertEquals(BitsOfInteger.reverse_bits(1023), 1023);
-        assertEquals(BitsOfInteger.reverse_bits(1024), 1);
+        Assertions.assertEquals(BitsOfInteger.reverse_bits(417), 267);
+        Assertions.assertEquals(BitsOfInteger.reverse_bits(267), 417);
+        Assertions.assertEquals(BitsOfInteger.reverse_bits(0), 0);
+        Assertions.assertEquals(BitsOfInteger.reverse_bits(2017), 1087);
+        Assertions.assertEquals(BitsOfInteger.reverse_bits(1023), 1023);
+        Assertions.assertEquals(BitsOfInteger.reverse_bits(1024), 1);
     }
 }
